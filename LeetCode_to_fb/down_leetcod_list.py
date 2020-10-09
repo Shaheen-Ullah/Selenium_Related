88c55b2f516e2d9d909267ee4d652880e4782ac8
yaming8002 <yamingworking@gmail.com>
2020-10-09 11:25:30 +0800

    first selenium

---
+++ LeetCode_to_fb/down_leetcod_list.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import time
+import os
+
+this_path = os.path.abspath(os.path.dirname(__file__))
+print(this_path)
+prefs={'profile.managed_default_content_settings.omages':2}
+options = webdriver.ChromeOptions()
+options.add_experimental_option('prefs',prefs)
+browser = webdriver.Chrome(executable_path=this_path+'/chromedriver' )
+    # options=options)
+
+
+url = 'https://leetcode.com/problemset/all/'
+browser.get(url)
+text = browser.page_source
+print(text)
+time.sleep(30)
+
+browser.quit()
